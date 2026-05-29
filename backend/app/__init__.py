235b3ac2dae1dd5a4809bdb5c565b4cc56f82b56
Алексей Сафонов <alexey@safonov.pro>
2026-05-29 23:40:41 +0300

    llm: исправила свои косяки работы с websocket-ами

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
 db = SQLAlchemy()
-socketio = SocketIO(cors_allowed_origins="*", async_mode="gevent")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="threading")
 
 def create_app():
     app = Flask(__name__)
